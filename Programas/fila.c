0c20730c702d515116b0f9ed649f6c453276845b
Maira Cavalheiro <mairadanielec@gmail.com>
2017-06-13 17:00:31 -0400

    fila
    adição de funcão de busca e remoção de todos os elementos

--- Programas/fila.c
+++ Programas/fila.c
@@ -40,7 +40,7 @@ void imprimir(fila *f){
   node* aux = f->inicio;
 
   if(aux == NULL){
-    printf("Fila vazia\n");
+    printf("Impressao: Fila vazia");
   }
 
   while (aux != NULL) {
@@ -63,6 +63,61 @@ void removerDuplaEnc(fila *f){
   free(temp);
 }
 
+void buscaOtimizada(fila *f, int value){
+  node *auxIni = f->inicio;
+  node *auxFim = f->fim;
+
+  if(auxIni == NULL && auxFim == NULL){
+    printf("Busca: Fila vazia\n");
+    return;
+  }
+
+  if(auxIni == auxFim){ // apenas um elemento na fila
+    printf("Valor encontrado \n");
+    return;
+  }
+
+  while (auxIni != auxFim) {
+    if(auxIni->chave != value){
+      auxIni = auxIni->prox;
+    }
+    else{
+      printf("Valor encontrado \n");
+      return;
+    }
+
+    if(auxFim->chave != value){
+      auxFim = auxFim->ant;
+    }
+    else{
+      printf("Valor encontrado \n");
+      return;
+    }
+  }
+
+    if(auxIni == auxFim){ // caso os ponteiros se encontrem
+      printf("Valor %d nao encontrado\n",value);
+      return;
+    }
+
+}
+
+void removerTodos(fila *f){ // remove todos os elementos da fila
+  node *aux = f->inicio;
+
+  while (aux != f->fim) { // laço que remove até o penultimo elemento
+    aux = aux->prox;
+    f->inicio = f->inicio->prox;
+    f->inicio->ant = NULL;
+  }
+
+  if(f->inicio == f->fim){// remove o ultimo elemento
+    f->inicio = f->fim = NULL;
+  }
+  free(aux);
+}
+
+
 int main(){
   fila *F1 = (fila*)malloc(sizeof(fila));
 
@@ -70,11 +125,19 @@ int main(){
   insereDuplamente(2, F1);
   insereDuplamente(10, F1);
   insereDuplamente(7, F1);
+  insereDuplamente(8, F1);
   insereDuplamente(15, F1);
   insereDuplamente(3, F1);
   imprimir(F1);
-  removerDuplaEnc(F1);
+  buscaOtimizada(F1,8);
+  removerDuplaEnc(F1); // remover um elemento
   imprimir(F1);
+  removerTodos(F1); // remover todos os elementos da fila
+  imprimir(F1);
+  insereDuplamente(2, F1);
+  imprimir(F1);
+
+  free(F1);
 
   return 0;
 }
